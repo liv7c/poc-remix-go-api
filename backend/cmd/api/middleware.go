dd86a35b08433b8bfaf481f7068f89ead451d19e
Olivia Coumans <livie7c@gmail.com>
2022-05-11 07:59:52 +0200

    feat(back): create enableCORS middleware

---
+++ backend/cmd/api/middleware.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+
+// enableCORS is a middleware to avoid CORS errors
+func (app *application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		next.ServeHTTP(w, r)
+	})
+}
